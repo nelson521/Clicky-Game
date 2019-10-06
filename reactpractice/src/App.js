29ce000cf8d3a04ee5b7171ce05fb6c4d8faa8d5
Stevenson Nelson <sonson521@gmail.com>
2019-10-06 15:16:14 -0400

    added nav

---
+++ reactpractice/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import './components/Nav'
+
+function App() { 
+  return <Nav />;
+}
+
+
+
+export default App;
---
+++ reactpractice/src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a class="navbar-brand" href="#">Navbar</a>
+        </li>
+      </ul>
+    </nav>
+  )
+    
+  
+}
+
+export default Nav; 
