16b4a2d7eb8e8b37318e7b51a68ddcf90c012566
Stevenson Nelson <sonson521@gmail.com>
2019-10-21 21:48:26 -0400

    added

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       }
     });
   }
-  // Map over this.state.cards and render a cardCard component for each card object
+  
   render() {
     return (
       <Wrapper>
